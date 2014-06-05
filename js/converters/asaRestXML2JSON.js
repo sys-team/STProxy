698d647429ca1a1f8ea4d4b1dfee29ac1ccb220c
DeadCat93 <alexey.i.panshin@gmail.com>
2014-06-05 16:09:15 +0400

    tag error in asaRestXML2JSON

--- js/converters/asaRestXML2JSON.js
+++ js/converters/asaRestXML2JSON.js
@@ -21,48 +21,55 @@ function convert(
     result["pageNumber"] = json["response"]["$"]["page-number"];
     result["pageRowCount"] = json["response"]["$"]["page-row-count"];
     
+    if (json["response"]["error"]) {
+        result["error"] = json["response"]["error"][0];
+    }
+    
     result["data"] = [];
     
-    json["response"]["d"].forEach(
-        function(obj){
-            var row = {};
-            var types = [];
-            var j = 0;
-            
-            //console.log(obj);
-            
-            row["name"] = obj["$"]["name"];
-            row["xid"] = obj["$"]["xid"];
-            row["properties"] = {};
-
-            for (var prop in obj) {
-                if (prop != "$"  && types.indexOf(prop == -1)) {
-                    types[j++] = prop;
-                }
-            }
-            
-            types.forEach(
-                function(name){
-                    obj[name].forEach(
-                        function(prop){
-                            //console.log(prop);
-                            
-                            if (prop["$"]["parent"]) {
-                                row[prop["$"]["name"]] = {};
-                                row[prop["$"]["name"]]["name"] = prop["$"]["parent"];
-                                row[prop["$"]["name"]]["xid"] = prop["$"]["parent-xid"];
-                                row[prop["$"]["name"]]["id"] = prop["_"];
-                            } else {
-                                row["properties"][prop["$"]["name"]] = prop["_"];
-                            }
-                        }
-                    )
-                }
-            );
-            
-            result["data"][i++] = row;   
-        });
+    if (json["response"]["d"]) {
+        
+        json["response"]["d"].forEach(
+            function(obj){
+                var row = {};
+                var types = [];
+                var j = 0;
+                
+                //console.log(obj);
+                
+                row["name"] = obj["$"]["name"];
+                row["xid"] = obj["$"]["xid"];
+                row["properties"] = {};
     
+                for (var prop in obj) {
+                    if (prop != "$"  && types.indexOf(prop == -1)) {
+                        types[j++] = prop;
+                    }
+                }
+                
+                types.forEach(
+                    function(name){
+                        obj[name].forEach(
+                            function(prop){
+                                //console.log(prop);
+                                
+                                if (prop["$"]["parent"]) {
+                                    row[prop["$"]["name"]] = {};
+                                    row[prop["$"]["name"]]["name"] = prop["$"]["parent"];
+                                    row[prop["$"]["name"]]["xid"] = prop["$"]["parent-xid"];
+                                    row[prop["$"]["name"]]["id"] = prop["_"];
+                                } else {
+                                    row["properties"][prop["$"]["name"]] = prop["_"];
+                                }
+                            }
+                        )
+                    }
+                );
+                
+                result["data"][i++] = row;   
+            });
+        
+    }   
     //console.log(result);
     
     str = JSON.stringify(result); 
