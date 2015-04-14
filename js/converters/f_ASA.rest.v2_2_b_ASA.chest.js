4137ec2124b076a0da65c62d3cc903eae4724f1e
DeadCat93 <alexey.i.panshin@gmail.com>
2015-04-14 17:09:22 +0300

    Emoji removing from values

--- js/converters/f_ASA.rest.v2_2_b_ASA.chest.js
+++ js/converters/f_ASA.rest.v2_2_b_ASA.chest.js
@@ -7,15 +7,15 @@ function convert(
     var result;
     var json;
     var xidRegexp = /^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$/;
-    
+
     if (!jsonString) {
         return '';
     }
 
     json = JSON.parse(jsonString);
-    
+
     result = xmlBuilder.create('post');
-        
+
     if (!options['titles']) {
 
         if (!(json instanceof Array)) {
@@ -23,51 +23,55 @@ function convert(
            arr[0] = json;
            json = arr;
         }
-        
+
         var keepArray = json;
         json = {};
         json['data'] = [];
         json['data'][options.entityName] = keepArray;
     }
-    
+
     for(var objArray in json['data']) {
-        
+
         if (json['data'][objArray] instanceof Array) {
-            
+
             json['data'][objArray].forEach(
             function(obj){
                 var record = result.ele((options.isPatch ? 'm' : 'd'));
-            
+
                 record.att('name', objArray);
-                
+
                 if (obj['id']) {
                     record.att('xid', obj['id']);
                 } else if (obj['xid']) {
                     record.att('xid', obj['xid']);
                 }
-                
+
                 if (options.isPatch && options.idAsParameter) {
                     record.att(options.idAsParameter, options.entityId);
                 }
-  
+
                 for (var prop in obj){
                     var attr;
-                        
+                    var p;
+
+                    p = obj[prop].replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '');
+
                     if (prop != 'xid' && prop != 'id') {
-                        if (xidRegexp.test(obj[prop])) {
+                        if (xidRegexp.test(p)) {
                             attr = record.ele('d');
                             attr.att('name', prop);
-                            attr.att('xid', obj[prop]);
+                            attr.att('xid', p);
                         } else {
-                            attr = record.ele((isNaN(obj[prop]) ? 'string' : 'double'), obj[prop]);
+
+                            attr = record.ele((isNaN(p) ? 'string' : 'double'), p);
                             attr.att('name', prop);
                         }
                     }
                 }
-        
+
             });
         }
-   
+
     };
 
     return result.toString();
