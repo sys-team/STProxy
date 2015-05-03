951aa1242485a749fadf3a6d3a017704e10683e7
DeadCat93 <alexey.i.panshin@gmail.com>
2015-05-03 18:36:44 +0300

    UOAuth error processing

--- js/converters/b_UOAuth.roles_2_f_UOAuth.roles.js
+++ js/converters/b_UOAuth.roles_2_f_UOAuth.roles.js
@@ -10,6 +10,7 @@ function convert(
     var attr = {};
     var resultObj = {};
     var parser = new xml2js.Parser();
+    var error;
     var account;
     var roles;
 
@@ -20,63 +21,74 @@ function convert(
         }
     );
 
-    result.account = {};
-    account = json.response.account[0];
+    //console.log(json);
 
-    Object.keys(account).forEach(
-        function(key){
-            result.account[key] = account[key][0];
-        }
-    );
+    if (json.response.error) {
+        error = json.response.error[0]
+    }
 
-    result.roles = {};
-    roles = json.response.roles[0].role;
+    if (error) {
+        result.error = error;
+    } else {
 
-    roles.forEach(
-        function(role){
+        result.account = {};
+        account = json.response.account[0];
 
-            if (role.data) {
+        Object.keys(account).forEach(
+            function(key){
+                result.account[key] = account[key][0];
+            }
+        );
 
-                var parsed;
+        result.roles = {};
+        roles = json.response.roles[0].role;
 
-                try {
-                    parsed = JSON.parse(role.data[0]);
-                } catch (err) {
-                    parsed = role.data[0]
-                };
+        roles.forEach(
+            function(role){
 
-                if (result.roles[role.code]){
+                if (role.data) {
 
-                    if (util.isArray(result.roles[role.code])){
+                    var parsed;
 
-                        result.roles[role.code].push(parsed)
+                    try {
+                        parsed = JSON.parse(role.data[0]);
+                    } catch (err) {
+                        parsed = role.data[0]
+                    };
+
+                    if (result.roles[role.code]){
+
+                        if (util.isArray(result.roles[role.code])){
+
+                            result.roles[role.code].push(parsed)
+
+                        } else {
+
+                            var val;
+
+                            val = result.roles[role.code];
+                            result.roles[role.code] = [];
+                            result.roles[role.code].push(val);
+                            result.roles[role.code].push(parsed);
+
+                        }
 
                     } else {
 
-                        var val;
-
-                        val = result.roles[role.code];
-                        result.roles[role.code] = [];
-                        result.roles[role.code].push(val);
-                        result.roles[role.code].push(parsed);
+                        result.roles[role.code] = parsed
 
                     }
 
                 } else {
-
-                    result.roles[role.code] = parsed
-
+                    result.roles[role.code] = true
                 }
 
-            } else {
-                result.roles[role.code] = true
             }
 
-        }
+        );
 
-    );
-
-    attr['page-row-count'] = 1;
+        attr['page-row-count'] = 1;
+    }
 
     resultObj.data = JSON.stringify(result);
     resultObj.attributes = attr;
