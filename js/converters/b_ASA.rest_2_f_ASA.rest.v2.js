92c46e64ea2e8ae9264776d9cefc81cf9c8ba29b
Alexander Levin <alevin.ru@gmail.com>
2015-06-12 15:44:44 +0300

    spaces

--- js/converters/b_ASA.rest_2_f_ASA.rest.v2.js
+++ js/converters/b_ASA.rest_2_f_ASA.rest.v2.js
@@ -2,158 +2,151 @@ var xml2js  = require('xml2js');
 var emoji = require('../emoji');
 
 function convert(
-    xml,
-    options
+  xml,
+  options
 ) {
-    var json;
-    var result = {};
-    var resultObj = {};
-    var attr = {};
-    var parser = new xml2js.Parser();
+  var json;
+  var result = {};
+  var resultObj = {};
+  var attr = {};
+  var parser = new xml2js.Parser();
 
-    parser.parseString(xml,
-        function(err, res){
-            json = res;
-        });
-
-    Object.keys(json['response']['$']).forEach(function(key){
-        attr[key] = json['response']['$'][key];
-        if (options['metadata']) {
-           result[key] = json['response']['$'][key];
-        }
+  parser.parseString(xml,
+    function(err, res){
+      json = res;
     });
 
-    if (json['response']['error']) {
-
-        if (json['response']['error'][0]['$']) {
-            result['error'] = json['response']['error'][0]['$']['code'];
-        } else {
-            result['error'] = json['response']['error'][0];
-        }
+  Object.keys(json['response']['$']).forEach(function(key){
+    attr[key] = json['response']['$'][key];
+    if (options['metadata']) {
+       result[key] = json['response']['$'][key];
     }
+  });
 
-    result = processXml(json, options, 0);
+  if (json['response']['error']) {
 
-
-    resultObj['data'] = JSON.stringify(result);
-    if (!options['titles']){
-        resultObj['dataArray'] = result['array'];
+    if (json['response']['error'][0]['$']) {
+      result['error'] = json['response']['error'][0]['$']['code'];
+    } else {
+      result['error'] = json['response']['error'][0];
     }
+  }
 
-    resultObj['attributes']  = attr;
+  result = processXml(json, options, 0);
 
-    return resultObj;
+
+  resultObj['data'] = JSON.stringify(result);
+  if (!options['titles']){
+    resultObj['dataArray'] = result['array'];
+  }
+
+  resultObj['attributes']  = attr;
+
+  return resultObj;
 }
 
 
 function processXml(
-    json,
-    options,
-    level
+  json,
+  options,
+  level
 ){
-    var resultObj = {};
+  var resultObj = {};
 
-    //console.log('level:');
-    //console.log(level);
-    //console.log('json');
-    //console.log(JSON.stringify(json));
+  //console.log('level:');
+  //console.log(level);
+  //console.log('json');
+  //console.log(JSON.stringify(json));
 
-    Object.keys(json).forEach(function(jkey) {
+  Object.keys(json).forEach(function(jkey) {
 
-        if (!options['titles']) {
-            resultObj['array'] = [];
+    if (!options['titles']) {
+      resultObj['array'] = [];
+    }
+
+    //console.log(jkey);
+    if (json[jkey].d) json[jkey].d.forEach(function(obj){
+
+      var row = {};
+      var types = [];
+
+      //console.log(JSON.stringify(obj));
+
+      if (!resultObj[obj['$']['name']] && options['titles']) {
+        resultObj[obj['$']['name']] = [];
+      }
+
+
+      if (typeof obj['_'] != 'string') {
+
+        for (var prop in obj) {
+          if (prop != '$'  && types.indexOf(prop == -1)) {
+            types.push(prop);
+          }
         }
 
-        //console.log(jkey);
-        if (json[jkey].d){
-            json[jkey].d.forEach(
-                function(obj){
-                    var row = {};
-                    var types = [];
+        if (obj.$) {
+          row['id'] = obj['$']['xid'];
+        }
 
-                    //console.log(JSON.stringify(obj));
+        types.forEach(function(name){
+          obj[name].forEach(function(prop){
+            //console.log('prop:');
+            //console.log(JSON.stringify(prop));
 
-                    if (!resultObj[obj['$']['name']]
-                    && options['titles']) {
-                        resultObj[obj['$']['name']] = [];
-                    }
+            if (prop['$']['parent']) {
 
+              row[prop['$']['name']] = prop['$']['parent-xid'];
 
-                    if (typeof obj['_'] != 'string') {
+            } else if(name == 'xml') {
 
-                        for (var prop in obj) {
-                            if (prop != '$'  && types.indexOf(prop == -1)) {
-                                types.push(prop);
-                            }
-                        }
+              Object.keys(prop[prop['$']['name']][0]).forEach(function(xkey){
+                var xmlPrep = {};
+                var tmp;
 
-                        if (obj.$) {
-                            row['id'] = obj['$']['xid'];
-                        }
+                if (xkey == 'd') {
+                  xmlPrep =  prop[prop['$']['name']];
+                } else {
+                  xmlPrep[xkey] = prop[prop['$']['name']][0][xkey][0];
+                }
 
-                        types.forEach(
-                            function(name){
-                                obj[name].forEach(
-                                    function(prop){
-                                        //console.log('prop:');
-                                        //console.log(JSON.stringify(prop));
+                row[prop['$']['name']] = processXml(xmlPrep, options, level +1).array;
 
-                                        if (prop['$']['parent']) {
-
-                                            row[prop['$']['name']] = prop['$']['parent-xid'];
-
-                                        } else if(name == 'xml'){
-
-                                            Object.keys(prop[prop['$']['name']][0]).forEach(function(xkey){
-                                                var xmlPrep = {};
-                                                var tmp;
-
-                                                if (xkey == 'd') {
-                                                    xmlPrep =  prop[prop['$']['name']];
-                                                } else {
-                                                    xmlPrep[xkey] = prop[prop['$']['name']][0][xkey][0];
-                                                }
-
-                                                row[prop['$']['name']] = processXml(xmlPrep, options, level +1).array;
-
-                                            });
+              });
 
 
-                                        } else if(prop['$']['name'] != 'id') {
+            } else if(prop['$']['name'] != 'id') {
 
-                                            if (prop['_']) {
-                                                row[prop['$']['name']] = emoji.unEscape(prop['_']);
-                                            }
-                                            if (prop.$.xid){
-                                                row[prop['$']['name']] = prop.$.xid;
-                                            }
-                                        }
-                                    }
-                                )
-                            }
-                        );
-
-                    } else {
-                        row[obj['$']['xid']] = obj['_'];
-                    }
-
-                    //console.log('row:');
-                    //console.log(JSON.stringify(row));
-
-                    if (options['titles']) {
-                        resultObj[obj['$']['name']].push(row);
-                    } else {
-                        resultObj['array'].push(row);
-                    }
-
-                });
+              if (prop['_']) {
+                row[prop['$']['name']] = emoji.unEscape(prop['_']);
+              }
+              if (prop.$.xid){
+                row[prop['$']['name']] = prop.$.xid;
+              }
             }
+          })
+        });
+
+      } else {
+        row[obj['$']['xid']] = obj['_'];
+      }
+
+      //console.log('row:');
+      //console.log(JSON.stringify(row));
+
+      if (options['titles']) {
+        resultObj[obj['$']['name']].push(row);
+      } else {
+        resultObj['array'].push(row);
+      }
+
     });
+  });
 
-    //console.log('resultObj:');
-    //console.log(resultObj);
+  //console.log('resultObj:');
+  //console.log(resultObj);
 
-    return resultObj;
+  return resultObj;
 
 }
 
